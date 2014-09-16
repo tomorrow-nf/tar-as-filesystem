96a5c436ad676769d8b9e8f2de0b3f8b2a852332
KDavi <kpdavidson@wpi.edu>
2014-09-16 16:48:13 -0400

    added analyze program file

---
+++ analyze_tar.c
@@ -0,0 +1,9 @@
+/*
+    Will accept a tar file name and print out the files in the archive as well as their
+    byte offsets and sizes (in bytes).
+*/
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+}
